4008b8e6c1fc1c261319acaae01e5ab5ba2bc28b
kelsbr <kelsbr@colostate.edu>
2023-11-28 22:20:34 -0700

    11/28/23-2020

--- src/main/java/RegressorPredictor/Predictor.java
+++ src/main/java/RegressorPredictor/Predictor.java
@@ -1,15 +1,25 @@
 package RegressorPredictor;
-import org.apache.spark.SparkConf;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.regression.RandomForestRegressor;
-import org.apache.spark.ml.regression.RandomForestRegressionModel;
+import org.apache.spark.ml.feature.OneHotEncoder;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import scala.Tuple1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Predictor {
+
 
     public static void main(String[] args) {
 
@@ -20,47 +30,77 @@ public class Predictor {
         // Initialize Spark
 
         SparkSession spark = SparkSession.builder().
-                appName("Predictor").master("local")
+                appName("Predictor")
                 .getOrCreate();
 
         JavaSparkContext jsc = JavaSparkContext.fromSparkContext(spark.sparkContext());
 
         // Load data (replace "path/to/your/data" with the actual path)
-        String dataPath = "/test_output/part_r_00000.";
-        Dataset<Row> data = spark.read().csv(inputPath);
+        String[] featureColumns = {"userID", "purchaseHistory", "nextPurchase", "score"};
+        Dataset<Row> data = spark.read().option("delimiter","/").csv(inputPath).drop("_c1").toDF(featureColumns);
+
+        JavaRDD<List<String>> histories = data.javaRDD()
+                .map(row -> row.getString(1).replaceAll("\\[|\\]", ""))
+                .map(h -> Arrays.asList(h.split(",")));
+
+        JavaRDD<Object> ids = histories.map(h ->{
+            ArrayList<String> list = new ArrayList<>();
+            for(String s : h){
+                list.add(s.split(":")[0]);
+            }
+            return list;
+        }).map(RowFactory::create);
+
+        JavaRDD<Object> scores = histories.map(h ->{
+            ArrayList<Float> list = new ArrayList<>();
+            for(String s : h){
+                list.add(Float.parseFloat(s.split(":")[1]));
+            }
+            return list;
+        }).map(RowFactory::create);
+
+        ids.take(10).forEach(h -> System.out.println(h.toString()));
+        scores.take(10).forEach(h -> System.out.println(h.toString()));
+
+
+
+
 
         // Split the data into training and test sets
         Dataset<Row>[] splits = data.randomSplit(new double[]{0.7, 0.3});
         Dataset<Row> trainingData = splits[0];
         Dataset<Row> testData = splits[1];
 
-        // Define the feature columns
-        String[] featureColumns = {"userID", "reviewerName", "purchaseHistory", "nextPurchase", "score"};
 
         // Assemble the feature columns into a single vector column
-        VectorAssembler assembler = new VectorAssembler()
-                .setInputCols(featureColumns)
-                .setOutputCol("features");
+//        VectorAssembler assembler = new VectorAssembler()
+//                .setInputCols(featureColumns)
+//                .setOutputCol("features");
 
-        Dataset<Row> assembledTrainingData = assembler.transform(trainingData);
-        Dataset<Row> assembledTestData = assembler.transform(testData);
+//
+//        Dataset<Row> assembledTrainingData = assembler.transform(trainingData);
+//        Dataset<Row> assembledTestData = assembler.transform(testData);
+//
+//        assembledTrainingData.show();
+//        assembledTestData.show();
 
-        // Create a RandomForestRegressor
-        RandomForestRegressor rf = new RandomForestRegressor()
-                .setLabelCol("label")
-                .setFeaturesCol("features")
-                .setNumTrees(10); // Number of trees in the forest
-
-        // Train the model
-        RandomForestRegressionModel model = rf.fit(assembledTrainingData);
-
-        // Make predictions on the test data
-        Dataset<Row> predictions = model.transform(assembledTestData);
-
-        // Show the predictions
-        predictions.select("prediction", "label", "features").show();
-
-        // Stop Spark
+//
+//        // Create a RandomForestRegressor
+//        RandomForestRegressor rf = new RandomForestRegressor()
+//                .setLabelCol("label")
+//                .setFeaturesCol("features")
+//                .setNumTrees(10); // Number of trees in the forest
+//
+//        // Train the model
+//        RandomForestRegressionModel model = rf.fit(assembledTrainingData);
+//
+//        // Make predictions on the test data
+//        Dataset<Row> predictions = model.transform(assembledTestData);
+//
+//        // Show the predictions
+//        predictions.select("prediction", "label", "features").show();
+//
+//        // Stop Spark
         spark.stop();
     }
 }
